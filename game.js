928470e811300dcec5b7453e39d5a6eb4d6866d2
DanielTwito <Daniel.twito7@gmail.com>
2019-04-21 16:52:50 +0300

    add classes

--- game.js
+++ game.js
@@ -1,19 +1,133 @@
 var context = canvas.getContext("2d");
-var shape = new Object();
 var board;
 var score;
+var lives;
 var pac_color;
 var pacman;
 var start_time;
 var time_elapsed;
 var interval;
 
-window.addEventListener("load", Start, false);
 
+
+class Charecter {
+    constructor(x,y){
+        this.x = x;
+        this.y = y;
+    }
+
+    getX(){
+        return x;
+    }
+
+    getY(){
+        return y;
+    }
+
+    setPosition(x,y) {
+        this.x = x;
+        this.y = y;
+    }
+
+
+    Draw(context){
+
+    }
+
+}
+
+class Food extends Charecter{
+
+    constructor(x,y,color){
+        super(x,y);
+        this.color=color
+    }
+
+    getScore(){
+        return 1;
+
+    }
+    Draw(context) {
+        var center = {};
+        center.x = this.x * 60 + 30;
+        center.y = this.y * 60 + 30;
+        context.beginPath();
+        context.arc(center.x, center.y, 15, 0, 2 * Math.PI); // circle
+        context.fillStyle = this.color; //color
+        context.fill();
+    }
+}
+
+class Wall extends Charecter{
+    constructor(x,y){
+        super(x, y);
+    }
+    Draw(context) {
+        var center = {};
+        center.x = this.x * 60 + 30;
+        center.y = this.y * 60 + 30;
+        context.beginPath();
+        context.rect(center.x - 30, center.y - 30, 60, 60);
+        context.fillStyle = "grey"; //color
+        context.fill();
+    }
+
+}
+class Pacman extends Charecter{
+
+    direction;
+
+    constructor(x,y,color){
+        super(x,y);
+        this.color=color;
+
+    }
+
+    Draw(context) {
+        var center = {};
+        center.x = this.x * 60 + 30;
+        center.y = this.y * 60 + 30;
+        context.beginPath();
+        context.arc(center.x, center.y, 30, 0.15 * Math.PI, 1.85 * Math.PI); // half circle
+        context.lineTo(center.x, center.y);
+        context.fillStyle = this.color; //color
+        context.fill();
+        context.beginPath();
+        context.arc(center.x + 5, center.y - 15, 5, 0, 2 * Math.PI); // circle
+        context.fillStyle = "black"; //color
+        context.fill();
+    }
+}
+
+class Ghost extends Charecter{
+    constructor(x,y){
+        super(x, y);
+    }
+
+    Draw(context) {
+    }
+}
+
+class Bonus extends Charecter{
+    constructor(x,y){
+        super(x, y);
+    }
+    Draw(context) {
+
+    }
+}
+
+
+
+
+
+
+window.addEventListener("load", Start, false);
 
 function Start() {
     board = new Array();
     score = 0;
+    lives = 3;
     pac_color = "yellow";
     var cnt = 100;
     var food_remain = 50;
@@ -24,19 +138,18 @@ function Start() {
         //put obstacles in (i=3,j=3) and (i=3,j=4) and (i=3,j=5), (i=6,j=1) and (i=6,j=2)
         for (var j = 0; j < 10; j++) {
             if ((i === 3 && j === 3) || (i === 3 && j === 4) || (i === 3 && j === 5) || (i === 6 && j === 1) || (i === 6 && j === 2)) {
-                board[i][j] = 4;
+                board[i][j] =new Wall(i,j);
             } else {
                 var randomNum = Math.random();
                 if (randomNum <= 1.0 * food_remain / cnt) {
                     food_remain--;
-                    board[i][j] = 1;
+                    board[i][j] = new Food(i,j,"black");
                 } else if (randomNum < 1.0 * (pacman_remain + food_remain) / cnt) {
-                    shape.i = i;
-                    shape.j = j;
                     pacman_remain--;
-                    board[i][j] = 2;
+                    board[i][j] = new Pacman(i,j,pac_color);
+                    pacman= board[i][j];
                 } else {
-                    board[i][j] = 0;
+                    board[i][j] = null;
                 }
                 cnt--;
             }
@@ -44,7 +157,7 @@ function Start() {
     }
     while (food_remain > 0) {
         var emptyCell = findRandomEmptyCell(board);
-        board[emptyCell[0]][emptyCell[1]] = 1;
+        board[emptyCell[0]][emptyCell[1]] = new Food(emptyCell[0],emptyCell[1],"black");
         food_remain--;
     }
 
@@ -55,14 +168,14 @@ function Start() {
     addEventListener("keyup", function (e) {
         keysDown[e.code] = false;
     }, false);
-    interval = setInterval(mainLoop, 100);
+    interval = setInterval(mainLoop, 90);
 }
 
 
 function findRandomEmptyCell(board) {
     var i = Math.floor((Math.random() * 9) + 1);
     var j = Math.floor((Math.random() * 9) + 1);
-    while (board[i][j] !== 0) {
+    while (board[i][j] !== null) {
         i = Math.floor((Math.random() * 9) + 1);
         j = Math.floor((Math.random() * 9) + 1);
     }
@@ -74,19 +187,15 @@ function findRandomEmptyCell(board) {
  */
 function GetKeyPressed() {
     if (keysDown['ArrowUp']) {
-        // keysDown['ArrowUp']=false;
         return 1;
     }
     if (keysDown['ArrowDown']) {
-        // keysDown['ArrowDown']=false;
         return 2;
     }
     if (keysDown['ArrowLeft']) {
-        // keysDown['ArrowLeft']=false;
         return 3;
     }
     if (keysDown['ArrowRight']) {
-        // keysDown['ArrowRight']=false;
         return 4;
     }
 }
@@ -98,69 +207,50 @@ function Draw() {
     context.clearRect(0, 0, canvas.width, canvas.height); //clean board
     lblScore.value = score;
     lblTime.value = time_elapsed;
+    lblLives.value = lives;
     for (var i = 0; i < 10; i++) {
         for (var j = 0; j < 10; j++) {
-            var center = new Object();
-            center.x = i * 60 + 30;
-            center.y = j * 60 + 30;
-            if (board[i][j] === 2) {
-                context.beginPath();
-                context.arc(center.x, center.y, 30, 0.15 * Math.PI, 1.85 * Math.PI); // half circle
-                context.lineTo(center.x, center.y);
-                context.fillStyle = pac_color; //color
-                context.fill();
-                context.beginPath();
-                context.arc(center.x + 5, center.y - 15, 5, 0, 2 * Math.PI); // circle
-                context.fillStyle = "black"; //color
-                context.fill();
-            } else if (board[i][j] === 1) {
-                context.beginPath();
-                context.arc(center.x, center.y, 15, 0, 2 * Math.PI); // circle
-                context.fillStyle = "black"; //color
-                context.fill();
-            } else if (board[i][j] === 4) {
-                context.beginPath();
-                context.rect(center.x - 30, center.y - 30, 60, 60);
-                context.fillStyle = "grey"; //color
-                context.fill();
-            }
-        }
+            if(board[i][j]!==null)
+                board[i][j].Draw(context);
+    }
     }
 
 
 }
 
 function UpdatePosition() {
-    board[shape.i][shape.j] = 0;
+    board[pacman.x][pacman.y] = null;
     var x = GetKeyPressed();
     if (x === 1) {
-        if (shape.j > 0 && board[shape.i][shape.j - 1] !== 4) {
-            shape.j--;
+        if (pacman.y > 0 && !(board[pacman.x][pacman.y - 1] instanceof Wall)) {
+            pacman.y--;
         }
     }
     if (x === 2) {
-        if (shape.j < 9 && board[shape.i][shape.j + 1] !== 4) {
-            shape.j++;
+        if (pacman.y < 9 && !(board[pacman.x][pacman.y + 1] instanceof Wall)) {
+            pacman.y++;
         }
     }
     if (x === 3) {
-        if (shape.i > 0 && board[shape.i - 1][shape.j] !== 4) {
-            shape.i--;
+        if (pacman.x > 0 && !(board[pacman.x-1][pacman.y] instanceof Wall)) {
+            pacman.x--;
         }
     }
     if (x === 4) {
-        if (shape.i < 9 && board[shape.i + 1][shape.j] !== 4) {
-            shape.i++;
+        if (pacman.x < 9 && !(board[pacman.x+1][pacman.y] instanceof Wall)) {
+            pacman.x++;
         }
     }
-    if (board[shape.i][shape.j] === 1) {
-        score++;
+
+    if (board[pacman.x][pacman.y] instanceof Food) {
+        score+=board[pacman.x][pacman.y].getScore();
     }
-    board[shape.i][shape.j] = 2;
+    board[pacman.x][pacman.y] = pacman;
+
     var currentTime = new Date();
     time_elapsed = (currentTime - start_time) / 1000;
     if (score >= 20 && time_elapsed <= 10) {
-        pac_color = "green";
+        pacman.color = "green";
     }
     if (score === 50) {
         window.clearInterval(interval);
